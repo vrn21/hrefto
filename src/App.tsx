aafc940bd23db5f4a9d84afe0ed9d86b56518258
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-01-16 17:59:50 +0530

    Frontend done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
+import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded';
 
 
 function App() {
-  
+  var [shortenedLink,setShortenedLink] = useState("wallet-hunter-vrn21s-projects.vercel.app/BestSongInDaWorld")  
   return (
     <main >
       <div className="flex justify-center w-[100vw] gap-[150px] items-center">
@@ -11,9 +14,27 @@ function App() {
           Unleash the Power of Short Links with  
           <span className="text-transparent animate-gradient bg-clip-text bg-[linear-gradient(to_right,theme(colors.yellow.300),theme(colors.yellow.400),theme(colors.yellow.500),theme(colors.yellow.500),theme(colors.yellow.600),theme(colors.yellow.700))]"> hrefto/</span>
         </h1>
-        <div className=" bg-[#FFFFFF1A] w-[470px] h-[450px] rounded-[60px] shadow-lg flex flex-col justify-center">
-          <Label htmlFor="link" className=" text-xl font-semibold">Your loooong link</Label>
-          <Input placeholder="" className="w-[350px]  h-12 text-lg"/>
+        <div className="  bg-[#FFFFFF1A] w-[470px] h-[450px] rounded-[60px] shadow-lg flex flex-col justify-center items-center ">
+          
+            <div>
+              <Label htmlFor="link" id="longlink" className=" text-xl font-semibold">Your loooong link:</Label>
+              <Input placeholder="https://youtu.be/dQw4w9WgXcQ?si=-14TnqpAB7Fsd3gc" className="w-[350px]  h-12 text-lg"/>
+            </div>
+            <div>
+              <Label htmlFor="text" id="shortened" className=" text-xl font-semibold">Shortened bit</Label>
+              <Input placeholder="BestSongInDaWorld" className="w-[350px]  h-12 text-lg"/>
+            </div>
+            <Button className=" bg-yellow-500 text-2xl font-semibold rounded-xl mt-6 hover:bg-yellow-300">Shorten!</Button>
+            <Label className="text-xl font-semibold mt-8">Your shortened link:</Label>
+            <div className=" flex flex-row items-center bordered text-xl border-2 rounded-xl border-[#FFFFFF4D] w-[350px] h-12 inline break-inside-avoid-column overflow-hidden text-overflow-ellipsis whitespace-nowrap"  id="shortlink">
+              <div className=" hover:bg-gray-600 w-7 h-8 m-1 ml-2 p-1 rounded-xl flex justify-center items-center">
+                <ContentCopyRoundedIcon/>
+              </div>
+              {shortenedLink}
+            </div>
+            
+          
+          
         </div>
       </div>
     </main>
