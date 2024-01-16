5c8af154749d1c4086dc7b43d27b1a2a5ff377da
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-01-16 17:07:35 +0530

    Skeleton donr

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main >
+      <div className="flex justify-center w-[100vw] gap-[150px] items-center">
+        <h1 className=" font-black max-w-[600px] text-[120px] leading-[0.8] text-gray-100">
+          Unleash the Power of Short Links with  
+          <span className="text-transparent animate-gradient bg-clip-text bg-[linear-gradient(to_right,theme(colors.yellow.300),theme(colors.yellow.400),theme(colors.yellow.500),theme(colors.yellow.500),theme(colors.yellow.600),theme(colors.yellow.700))]"> hrefto/</span>
+        </h1>
+        <div className=" bg-[#FFFFFF1A] w-[470px] h-[450px] rounded-[60px] shadow-lg flex flex-col justify-center">
+          <Label htmlFor="link" className=" text-xl font-semibold">Your loooong link</Label>
+          <Input placeholder="" className="w-[350px]  h-12 text-lg"/>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
